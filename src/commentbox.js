69ddbeaac250a711f84af0dc62b441c72564cb86
akkey247 <akkey52417@gmail.com>
2017-06-24 10:38:25 +0900

    React.jsリファレンスのチュートリアルを実験

---
+++ src/commentbox.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+export default class CommentBox extends Component {
+  render() {
+    return (
+      <div className="commentBox">
+        Hello, world! I am a CommentBox.
+      </div>
+    );
+  }
+}
--- src/script.js
+++ src/script.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import Counter from './counter.js';
+import CommentBox from './commentbox.js';
+import CommentList from './commentlist.js';
+import CommentForm from './commentform.js';
 
 window.App = {
   render: () => {
     ReactDom.render(
-      <Counter />,
+      <div className="commentBox">
+        <h1>Comments</h1>
+        <CommentList />
+        <CommentForm />
+      </div>,
       document.getElementById('root')
     );
   }
