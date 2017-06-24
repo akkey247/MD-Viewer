ac58e52354a027573bd5f44044b55777e654a12b
akkey247 <akkey52417@gmail.com>
2017-06-24 10:06:54 +0900

    React.js+Babelを利用できるように修正

--- src/script.js
+++ src/script.js
@@ -1,4 +1,12 @@
-var $ = require("jquery");
-$(function(){
-  console.log('test');
-});
+import React from 'react';
+import ReactDom from 'react-dom';
+import Counter from './counter.js';
+
+window.App = {
+  render: () => {
+    ReactDom.render(
+      <Counter />,
+      document.getElementById('root')
+    );
+  }
+};
--- webpack.config.js
+++ webpack.config.js
@@ -2,5 +2,18 @@ var path = require('path');
 module.exports = {
   output: {
     filename: 'script.js'
+  },
+  module: {
+    loaders: [
+      {
+        loader: 'babel',
+        exclude: /node_modules/,
+        test: /\.js[x]?$/,
+        query: {
+          cacheDirectory: true,
+          presets: ['react', 'es2015']
+        }
+      }
+    ]
   }
 }
