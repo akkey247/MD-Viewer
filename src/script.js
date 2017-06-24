a2b336eace154797a014ee618c430fde87940e3d
akkey247 <akkey52417@gmail.com>
2017-06-24 15:56:22 +0900

    表示するデータを別で取得するように修正

--- src/script.js
+++ src/script.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import Counter from './counter.js';
 import CommentBox from './commentbox.js';
+import {data} from './data.js';
 
 window.App = {
   render() {
     ReactDom.render(
-      <CommentBox />,
+      <CommentBox data={data} />,
       document.getElementById('root')
     );
   }
