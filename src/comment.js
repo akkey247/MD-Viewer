a52f83082963c893092839ab0ec142228bf63b45
akkey247 <akkey52417@gmail.com>
2017-06-24 15:12:06 +0900

    markedパッケージを入れて、MarkDownとして文字列読み込み表示するよう修正

--- src/comment.js
+++ src/comment.js
@@ -1,13 +1,15 @@
 import React, {Component} from 'react'
+import marked from 'marked';
 
 export default class Comment extends Component {
   render() {
+    var rawMarkup = marked(this.props.children.toString(), {sanitize: true});
     return (
       <div className="comment">
         <h2 className="commentAuthor">
           {this.props.author}
         </h2>
-        {this.props.children}
+        <span dangerouslySetInnerHTML={{__html: rawMarkup}} />
       </div>
     );
   }
