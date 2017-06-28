54616f341ffe068dfd33041e86f25588f4c1b16f
akkey247 <akkey52417@gmail.com>
2017-06-28 23:30:13 +0900

    versionの指定方法を変更

--- gulpfile.js
+++ gulpfile.js
@@ -31,13 +31,13 @@ gulp.task('default', function () {
 // パッケージ作成(Windows)
 gulp.task('package:win32', function (done) {
   packager({
-    dir: './',                // アプリケーションのパッケージとなるディレクトリ
-    out: './dist',            // .app や .exeの出力先ディレクトリ
-    name: 'ElectronApp',      // アプリケーション名
-    arch: 'x64',              // CPU種別. x64 or ia32
-    platform: 'win32',        // OS種別. darwin or win32 or linux
-    version: '1.4.13',        // Electronのversion
-    overwrite: true           // 上書き
+    dir: './',                 // アプリケーションのパッケージとなるディレクトリ
+    out: './dist',             // .app や .exeの出力先ディレクトリ
+    name: 'ElectronApp',       // アプリケーション名
+    arch: 'x64',               // CPU種別. x64 or ia32
+    platform: 'win32',         // OS種別. darwin or win32 or linux
+    electronVersion: '1.6.11', // Electronのversion
+    overwrite: true            // 上書き
   }, function (err, path) {
     // 追加でパッケージに手を加えたければ, path配下を適宜いじる
     done();
